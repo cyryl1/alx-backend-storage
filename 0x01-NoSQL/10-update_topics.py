1c27f7fd9de02588b6803ee33ec5e329d8995aed
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-23 07:53:48 +0100

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    Args:
+        mongo_collection (pymongo.collection.Collection): MongoDB collection
+        name: (string) school name to update
+        topics: (list of strings) new topics for the school
+    """
+    if not isinstance(name, str) or not isinstance(topics, list):
+        return
+    
+    result = mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return result.modified_count
