becf2e2fed34aa1805cb0c9b6cc6c4f5bd6c38d8
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-23 07:29:38 +0100

    Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new school document into the MongoDB collection.
+    Args:
+    mongo_collection (pymongo.collection.Collection): The MongoDB collection to insert the document into.
+    **kwargs: The school document fields and values to insert.
+    Returns:
+    _id of the newly inserted document
+    """
+
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
+
+    
